a4d9f62213f2cda3ce251cdf494ef2dfef6b277f
ValeLan <vale.lanf@gmail.com>
2025-02-09 21:11:32 -0300

    Cree primer formulario con react-hook-form con validaciones y un manejo de pacientes de estado glbal con zustand

---
+++ src/components/PatientList.tsx
@@ -0,0 +1,12 @@
+import { usePatientStore } from "../store"
+
+
+const PatientList = () => {
+    const patients = usePatientStore((state) => state.patients)
+  
+    return (
+    <div>PatientList</div>
+  )
+}
+
+export default PatientList
